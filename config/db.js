5784261b4cf85e7cd23bca5db44701dd0dc6fbcb
Mar1yam <145283925+Mar1yam@users.noreply.github.com>
2023-11-21 13:11:24 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,1 @@
-module.exports = {"URI":"mongodb+srv://mariyammember:325193688@cluster0.bzwdf0l.mongodb.net/assignmentTracker"}
+module.exports = {"URI":"mongodb://127.0.0.1:27017/assignmentTracker"}
